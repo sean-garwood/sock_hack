413935284862f8bb7238d805780b388915dd47d8
sean-garwood <seansgarwood@gmail.com>
2025-10-18 19:41:12 -0400

    Initial commit
    Recon
    * fcc reports, photos
    * packet analysis
    * web app

---
+++ README.md
@@ -0,0 +1,32 @@
+# Sock Hack
+
+Reverse engineering the [Owlet DreamSock 3](http://localhost)
+
+## Goals
+
+* Learn about
+  * reverse-engineering
+  * IoT devices
+  * embedded systems
+* Evaluate the security of the web service(s) running on the device
+* Modify the behavior of the device, if possible.
+
+## Findings
+
+The device comes with two components: a "sock", which consists of two devices--a
+bluetooth module and some kind of (IR?) sensor that presumably can read
+"pulseox"; and a "base station", which appears to act as an access point and
+[requires setup via a third-party app on a
+smartphone](./recon/docs/installation_manual.pdf)
+
+### Web application
+
+When powered, the base station appears to act as an access point. It does not
+appear to allow outbound connections (see various [packet
+captures](./recon/pcaps/)), except perhaps to call some API endpoint in the
+[aylanetworks domain](./recon/docs/ayla_networks_api_docs.txt).
+
+The [user interface](./app/index.html) can be accessed by connecting to the AP
+and navigating to 192.168.0.1. It displays a list of all available wifi
+networks, as well as connect buttons and stuff. It also loads [a pretty basic
+script](./app/wifi.js) that serves as the controller.
---
+++ app/wifi.js
@@ -0,0 +1,396 @@
+/**
+ * Draw interface from which the user selects the wifi network to "connect" to.
+ * @todo figure out what we're connecting to...
+ */
+
+function banner(a) {
+  document.writeln(
+    "<title>" +
+      a +
+      "</title></head><body><center><div id=status></div></center>"
+  );
+}
+
+function elem_set(b, a) {
+  document.getElementById(b).innerHTML = a;
+}
+
+function escapeHtml(a) {
+  let b = document.createElement("div");
+  b.appendChild(document.createTextNode(a));
+  return b.innerHTML;
+}
+
+function unescapeHtml(a) {
+  let c = document.createElement("div");
+  c.innerHTML = a;
+  let b = c.childNodes[0];
+  return b ? b.nodeValue : "";
+}
+
+function status_msg(c, b) {
+  let a = "Cancel";
+  if (b) {
+    a = "Dismiss";
+  }
+  elem_set(
+    "status",
+    "<div id=flash_alert>" +
+      c +
+      '<p><button onclick="conn_cancel()">' +
+      a +
+      "</button></div>"
+  );
+  document.getElementById("status").scrollIntoView(true);
+}
+
+function scan_td(a) {
+  return "<td>" + a + "</td>";
+}
+
+function ssid_or_bssid(a) {
+  if (a.bssid) {
+    return "bssid=" + encodeURIComponent(a.bssid);
+  }
+  return "ssid=" + encodeURIComponent(a.ssid);
+}
+
+function wifi_spec_status_resp(d, b) {
+  let c = window.scan_cur;
+  let i;
+  let g;
+  let h;
+  let a = 0;
+  if (b !== 200) {
+    return;
+  }
+  i = JSON.parse(d).wifi_status;
+  try {
+    g = i.connect_history[0];
+    if (g.last == 1 && g.error == 0) {
+      h = "Connection complete.";
+      a = 1;
+    } else {
+      if (g.last == 1) {
+        h = "Connection failed: " + g.msg + " (error " + g.error + ")";
+        a = 1;
+      } else {
+        h = "In progress";
+        if (i.state != null) {
+          h += ": " + i.state;
+        }
+      }
+    }
+  } catch (f) {}
+  status_msg("Connection to " + escapeHtml(c.ssid) + "<br>" + h, a);
+  if (a == 1) {
+    clearInterval(window.sts_intvl);
+  }
+  elem_set("div1", "");
+  elem_set("div2", "");
+}
+/**
+ * Fake an asynchronous request
+ *
+ * @param
+ */
+
+function send_async_req(httpRequestType, a, d, f) {
+  const req = new XMLHttpRequest();
+  let someFlag = false;
+  const cb = setTimeout(function () {
+    someFlag = true;
+    req.abort();
+  }, d);
+  req.open(httpRequestType, a, true);
+  req.onreadystatechange = function () {
+    if (req.readyState !== 4) {
+      return;
+    }
+    if (someFlag) {
+      return;
+    }
+    clearTimeout(cb);
+    f(req.responseText, req.status);
+  };
+  req.send(null);
+}
+
+function send_sync_req(d, a, c) {
+  let b = new XMLHttpRequest();
+  b.open(d, a, false);
+  b.send(null);
+  c(b.responseText, b.status);
+}
+
+function wifi_status_resp(b, a) {
+  window.wifi_status = JSON.parse(b).wifi_status;
+}
+
+/**
+ * Get the available wifi networks.
+ * @param statusFlag Determines whether to send a sync request.
+ */
+function wifi_status_get(statusFlag = 0) {
+  let b = "wifi_status.json";
+  let d = wifi_status_resp;
+  if (statusFlag) {
+    let a = window.scan_cur;
+    if (!a) {
+      return;
+    }
+    b += "?" + ssid_or_bssid(a);
+    d = wifi_spec_status_resp;
+    send_async_req("GET", b, 1000, d);
+    return;
+  }
+  send_sync_req("GET", b, d);
+}
+
+function wifi_connect_resp(b, a) {
+  if (a >= 200 && a < 300) {
+    window.sts_intvl = setInterval(wifi_status_get, 1000, 1);
+    status_msg("Status unknown, please wait.<br>", 0);
+  } else {
+    let c = JSON.parse(b);
+    if (c != null) {
+      status_msg("Error: " + c.msg, 0);
+    }
+  }
+}
+
+function conn_start(c, a) {
+  let b = "wifi_connect.json?" + ssid_or_bssid(a);
+  if (c != "") {
+    b += "&key=" + encodeURIComponent(c);
+  }
+  send_async_req("POST", b, 1000, wifi_connect_resp);
+}
+
+function conn_ok() {
+  let a = window.scan_cur;
+  try {
+    let d = document.getElementById("ssid");
+    if (d) {
+      a.ssid = d.value;
+    }
+    let b = document.getElementById("key");
+    if (b) {
+      conn_start(b.value, a);
+    }
+  } catch (c) {
+    console.error("caught " + c.message);
+  }
+  return false;
+}
+
+function conn_cancel() {
+  window.scan_cur = null;
+  elem_set("status", "");
+  redraw();
+}
+
+function conn_prompt() {
+  let a = window.scan_cur;
+  let b = "<caption>Connect to network</caption><tr><td>Network<td>";
+  if (a.ssid == "Join Other Network...") {
+    b +=
+      "<input id=ssid size=15 autocomplete=offautofocus autocapitalize=off autocorrect=offrequired>";
+  } else {
+    b += escapeHtml(a.ssid) + "<tr><td>Security<td>" + a.security;
+  }
+  b +=
+    '<tr><td>Password<td><input type=password id=key size=15 required autocapitalize=off autocorrect=off><tr><td><td><button type=button onclick="conn_cancel()">Cancel</button>&nbsp;&nbsp;&nbsp;<button>Connect</button>';
+  elem_set(
+    "div1",
+    '<form onSubmit="return conn_ok();"><table class=networks>' +
+      b +
+      "</table></form>"
+  );
+  elem_set("div2", "");
+}
+
+function connect(b) {
+  let a = window.scan_results[b];
+  window.scan_cur = a;
+  if (a.security != "None") {
+    conn_prompt();
+  } else {
+    conn_start("", a);
+  }
+}
+
+function scan_connect(a) {
+  return scan_td('<button onclick="connect(' + a + ')">Connect</button>');
+}
+
+function scan_line(b, a) {
+  let d = "<tr>";
+  d += scan_td(escapeHtml(a.ssid));
+  d += "<td><table id=wifi_bars><tbody><tr>";
+  for (let c = 0; c < a.bars; c++) {
+    d += "<td width=20px>&nbsp;";
+  }
+  d += "</tbody></table></td>";
+  if (a.type == "Ad hoc") {
+    d += scan_td("ad-hoc");
+  } else {
+    if (a.type != "AP") {
+      d += scan_td("-");
+    } else {
+      if (a.ssid == window.wifi_status.connected_ssid) {
+        d += scan_td("Connected");
+      } else {
+        d += scan_connect(b);
+      }
+      if (a.security != "None" && a.security != "Unknown") {
+        d += scan_td('<img alt=Secure src="lock.gif" style="height:0.92em;">');
+      }
+    }
+  }
+  d += "</tr>";
+  return d;
+}
+
+function scan_tab() {
+  let c = window.scan_results;
+  let b =
+    '<caption><h4>Select Wi-Fi Network<span id=refresh><image src=refresh.gif alt=Refresh style="height:0.92em;"onclick="rescan()"></span></h4></caption><thead><tr><th>Network</th><th>Strength</th><th class=conn_action></th></tr></thead><tbody>';
+  for (let a = 0; a < c.length; a++) {
+    b += scan_line(a, c[a]);
+  }
+  elem_set("div1", "<table class=networks>" + b + "</table>");
+}
+
+function wifi_scan_rslt_resp(b, a) {
+  let d;
+  try {
+    d = JSON.parse(b).wifi_scan.results;
+    d.sort(function (f, e) {
+      if (e.type != "AP") {
+        return f.signal;
+      }
+      if (f.type != "AP") {
+        return -e.signal;
+      }
+      if (f.signal == e.signal && f.ssid != e.ssid) {
+        return e.ssid < f.ssid ? 1 : -1;
+      }
+      return e.signal - f.signal;
+    });
+  } catch (c) {
+    d = new Array();
+  }
+  d.push({
+    ssid: "Join Other Network...",
+    bars: 0,
+    security: "Unknown",
+    type: "AP",
+  });
+  window.scan_results = d;
+  scan_tab();
+}
+
+function scan(b, a) {
+  send_async_req("GET", "wifi_scan_results.json", 1000, wifi_scan_rslt_resp);
+}
+
+function rescan() {
+  send_async_req("POST", "wifi_scan.json", 1000, scan);
+}
+
+function prof_del_resp(b, a) {
+  if (a < 299) {
+    status_msg("Delete successful", 1);
+  } else {
+    status_msg("Delete failed", 1);
+  }
+  update();
+}
+
+function del_ok(c) {
+  let b = unescapeHtml(c);
+  let a = "wifi_profile.json?ssid=" + encodeURIComponent(b);
+  send_async_req("DELETE", a, 1000, prof_del_resp);
+}
+
+function prof_delete(c) {
+  let a = "<table><caption>Confirm ";
+  let b = unescapeHtml(c);
+  if (b == window.wifi_status.connected_ssid) {
+    a += "disconnect and ";
+  }
+  a +=
+    "delete of network " +
+    c +
+    '</caption><tr><td><button type=button onclick="redraw()">Cancel</button><td><button type=button onclick="del_ok(&quot;' +
+    c +
+    '&quot;)">OK</button></table>';
+  elem_set("div1", a);
+  elem_set("div2", "");
+}
+
+function prof_line(b) {
+  let a;
+  html = "<tr><td>" + escapeHtml(b.ssid) + "<td>";
+  if (b.ssid == window.wifi_status.connected_ssid) {
+    html += "Connected";
+    a = "Disconnect";
+  } else {
+    a = "Delete";
+  }
+  return (
+    html +
+    '<td><button onclick="prof_delete(&quot;' +
+    escapeHtml(b.ssid) +
+    '&quot;)">' +
+    a +
+    "</button>"
+  );
+}
+
+function prof_tab(a) {
+  let d = window.proftab;
+  let c;
+  c =
+    "<table class=networks><caption><h4>Wi-Fi Profiles</h4></caption><thead><tr><th>Network<th>Status<th><th><tbody>";
+  if (d != null) {
+    d.sort(function (f, e) {
+      if (e.ssid == f.ssid) {
+        return 0;
+      }
+      return e.ssid < f.ssid ? 1 : -1;
+    });
+    for (let b = 0; b < d.length; b++) {
+      c += prof_line(d[b]);
+    }
+  }
+  c += "</table>";
+  elem_set("div2", c);
+}
+
+function wifi_prof_resp(b, a) {
+  window.proftab = JSON.parse(b).wifi_profiles;
+  prof_tab();
+}
+
+function profiles() {
+  send_async_req("GET", "wifi_profiles.json", 1000, wifi_prof_resp);
+}
+
+function redraw() {
+  scan_tab();
+  prof_tab();
+}
+
+function update() {
+  profiles();
+  scan();
+}
+
+wifi_status_get();
+banner(window.wifi_status.host_symname + " Wifi Status");
+document.writeln(
+  "<section><div class=centered><br><div id=div1></div><br><br><div id=div2></div>"
+);
+update();
